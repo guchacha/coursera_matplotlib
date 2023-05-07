f18114397b281bac0c66e53d8f15fb5cba5d35e4
Agnieszka Kowalczyk <gu.nowakowska@gmail.com>
2023-05-07 13:24:52 +0200

    [backend] (feat) implementing common data processing function and updating entry point file

--- src/app/core/input.py
+++ src/app/core/input.py
@@ -1,40 +1,35 @@
 import pandas as pd
 
 
-def data_import_and_processing():
-    tables_ch=pd.read_html("https://en.wikipedia.org/wiki/List_of_Chelsea_F.C._seasons")
-    df_ch = (tables_ch[2])
-    df_ch.columns = df_ch.columns.get_level_values(1)
-    df_ch = df_ch.iloc[33:109, 0:10].reset_index(drop=True)
+def data_import_and_processing(clubname: str, wikiobj: int, collevel: int) -> pd.DataFrame:
+    """
+    Function prepares dataframe with moving average of final position in the League of London FC.
+    :param clubname: name of FC from London
+    :param wikiobj: object number from wikipedia page
+    :param collevel: column name level
+    :return: dataframe with season name and moving average position of club
+    """
 
-    df_ch = df_ch[['Season', 'Pos']]
-    df_ch['Season'] = df_ch['Season'].str[:4]
-    df_ch['Pos'] = df_ch['Pos'].str[:-2]
-    df_ch.loc[41, 'Pos'] = 18
-    df_ch = df_ch.astype(int)
-    df_ch['Av10'] = df_ch['Pos'].rolling(10).mean()
+    # choose proper data from wikipedia
+    data = pd.read_html(f"https://en.wikipedia.org/wiki/List_of_{clubname}_seasons")
+    df = (data[wikiobj])
+    df.columns = df.columns.get_level_values(collevel)  # which level of columns name
+    df = df[['Season', 'Pos']]
 
-    tables_ar=pd.read_html("https://en.wikipedia.org/wiki/List_of_Arsenal_F.C._seasons")
-    df_ar = tables_ar[3]
-    df_ar.columns = df_ar.columns.get_level_values(0)
-    df_ar = df_ar.iloc[53:129, 0:10].reset_index(drop=True)
+    # dataframe cleaning
+    for cs in ["Competitive", "competitive", "Season"]:
+        df = df.loc[~df['Season'].str.contains(cs)]
+    for cp in ["season", "—", "W", "League"]:
+        df = df.loc[~df['Pos'].str.contains(cp)]
+    df = df.replace(to_replace=r"\[.*\]", value="", regex=True)
+    df['Season'] = df['Season'].str[:4]
+    df['Pos'] = df['Pos'].str[:-2]
+    df = df.drop_duplicates().reset_index(drop=True)
+    df = df.astype(int)
 
-    df_ar = df_ar[['Season', 'Pos']]
-    df_ar['Season'] = df_ar['Season'].str[:4]
-    df_ar['Pos'] = df_ar['Pos'].str[:-2]
-    df_ar = df_ar.astype(int)
-    df_ar['Av10'] = df_ar['Pos'].rolling(10).mean()
+    # calculate moving average
+    df = df.loc[df['Season'] > 1945]
+    df = df.reset_index(drop=True)
+    df['Av10'] = df['Pos'].rolling(10).mean()
 
-    tables_to=pd.read_html("https://en.wikipedia.org/wiki/List_of_Tottenham_Hotspur_F.C._seasons")
-    df_to = tables_to[0]
-    df_to.columns = df_to.columns.get_level_values(1)
-    df_to = df_to.iloc[75:160, 0:10]
-    df_to = df_to.drop_duplicates().reset_index(drop=True)
-
-    df_to = df_to[['Season', 'Pos']]
-    df_to['Season'] = df_to['Season'].str[:4]
-    df_to['Pos'] = df_to['Pos'].str[:-2]
-    df_to['Season'] = df_to['Season'].astype(int)
-    df_to['Pos'] = df_to['Pos'].astype(int)
-    df_to['Av10'] = df_to['Pos'].rolling(10).mean()
-    return df_ch, df_ar, df_to
+    return df
--- src/app/main.py
+++ src/app/main.py
@@ -2,5 +2,8 @@ from core.input import data_import_and_processing
 from core.output import data_plotting
 
 if __name__ == "__main__":
-    ch, ar, to = data_import_and_processing()
+    ch = data_import_and_processing(clubname="Chelsea_F.C.", wikiobj=2, collevel=1)
+    ar = data_import_and_processing(clubname="Arsenal_F.C.", wikiobj=3, collevel=0)
+    to = data_import_and_processing(clubname="Tottenham_Hotspur_F.C.", wikiobj=0, collevel=1)
+
     data_plotting(df_ch=ch, df_ar=ar, df_to=to)
